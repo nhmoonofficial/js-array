04a3057299f01e20998c9c7dbd2c5c923509e966
Nazmul-Moon <nhmoonofficial@gmail.com>
2025-05-18 11:43:19 +0600

    2nd task completed

--- array.js
+++ array.js
@@ -8,11 +8,28 @@
 // console log the final array
 
 
-var fruits = [ 'Mango', 'Jam', 'Jackfruits', 'Banana', 'Apple', 'Orange' ]
-console.log(fruits);
+// var fruits = [ 'Mango', 'Jam', 'Jackfruits', 'Banana', 'Apple', 'Orange' ]
+// console.log(fruits);
 
-console.log(fruits.indexOf('Jackfruits'));
+// console.log(fruits.indexOf('Jackfruits'));
 
 
-fruits[1] = 'jambura';
-console.log(fruits);
+// fruits[1] = 'jambura';
+// console.log(fruits);
+
+
+// 2. Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+
+const tourist = ['Cox', 'Bandarban', 'SaintMartin']
+
+tourist.push('Sylhet')
+console.log(tourist);
+tourist.push('Mawua','Rajshahi')
+tourist.pop();
+console.log(tourist);
